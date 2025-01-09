23d1d1a2aa61ae2bd67f4a5f33d91dd3f8436d04
nexos20levrai <156014354+nexos20levrai@users.noreply.github.com>
2025-01-09 22:08:14 +0100

    Ajout de la barre de volume

--- interface/scripts/SettingsDialog.gd
+++ interface/scripts/SettingsDialog.gd
@@ -2,12 +2,16 @@ extends DialogBody
 
 onready var camera_slider: HSlider = $CameraContainer/HBoxContainer/CameraSlider
 onready var camera_label: Label = $CameraContainer/HBoxContainer/CameraLabel
+onready var audio_slider: HSlider = $VolumeContainer/HBoxContainer/AudioSlider
+onready var audio_label: Label = $VolumeContainer/HBoxContainer/AudioLabel
+onready var master_bus = AudioServer.get_bus_index("Master")
 
 func _ready():
 	camera_slider.min_value = GameSettings.get_min_sensitivity()
 	camera_slider.max_value = GameSettings.get_max_sensitivity()
 	camera_slider.value = GameSettings.get_current_sensitivity()
 	camera_label.text = str(stepify(GameSettings.get_current_sensitivity(),0.01))
+	audio_slider.value = AudioServer.get_bus_volume_db(master_bus)
 
 func confirm_pressed():
 	GameSettings.set_current_sensitivity(camera_slider.value)
@@ -15,3 +19,15 @@ func confirm_pressed():
 
 func _on_CameraSlider_value_changed(value):
 	camera_label.text = str(stepify(value,0.01))
+
+func _on_AudioSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(master_bus, value)
+	
+	if value <= -30:
+		AudioServer.set_bus_mute(master_bus, true)
+		audio_label.text = "0%"
+	else:
+		AudioServer.set_bus_mute(master_bus, false)
+		# Calculer le pourcentage basé sur la plage de -30 à 0 dB
+		var percentage = int((value + 30) / 30.0 * 100)
+		audio_label.text = str(percentage) + "%"
